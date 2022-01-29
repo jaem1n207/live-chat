216fc05d0d85577ee50fe52fe2894459150bb0a9
jaem1n207 <roy.jm.lee@gmail.com>
2022-01-30 00:27:03 +0900

    Add: Logo

--- src/app/pages/HomePage/Features/GithubRepoForm/slice/index.ts
+++ src/app/pages/HomePage/Features/GithubRepoForm/slice/index.ts
@@ -6,7 +6,7 @@ import { githubRepoFormSaga } from './saga';
 import { GithubRepoFormState, RepoErrorType } from './types';
 
 export const initialState: GithubRepoFormState = {
-  username: 'react-boilerplate',
+  username: 'jaem1n207',
   repositories: [],
   loading: false,
   error: null,
